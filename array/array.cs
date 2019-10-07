1ec39270bb6918ef6a5f2127d4934a78a367aeca
Ethan Brierley <ethanboxx@gmail.com>
2019-10-07 14:11:22 +0100

    arrays

---
+++ array/array.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Array
+{
+    public static void Main(string[] args)
+    {
+        char[] arrayTest = new char[7];
+        arrayTest[0] = 'E';
+        arrayTest[1] = 'C';
+        arrayTest[2] = 'F';
+        arrayTest[3] = 'A';
+        arrayTest[4] = 'D';
+        arrayTest[5] = 'G';
+        display(arrayTest);
+        arrayTest[6] = 'M';
+        display(arrayTest);
+    }
+    private static void display(char[] arrayTest)
+    {
+        Console.WriteLine(arrayTest);
+        Console.WriteLine("Showing array elements");
+        for (int counter = 0; counter < arrayTest.Length; counter++)
+            Console.WriteLine("Index: {0}, Element: {1}", counter, arrayTest[counter]);
+
+    }
+}
