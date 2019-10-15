b0ad0a25a804aeb0c28f048d5c5c288203c5dbe2
Ethan Brierley <ethanboxx@gmail.com>
2019-10-15 09:21:01 +0100

    Add code

--- routine/Program.cs
+++ routine/Program.cs
@@ -6,7 +6,17 @@ namespace routine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input an number.");
+            int input = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(
+                "The number is equal to 5: {0}\nThe number is a multiple of 7 or 3: {1}",
+                equal(input, 5),
+                multiple_of_three_or_seven(input)
+            );
         }
+        static bool equal(int one, int two) =>
+            one == two;
+        static bool multiple_of_three_or_seven(int num) =>
+            (num % 3 == 0) || (num % 7 == 0);
     }
 }
