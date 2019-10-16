928b2b9c57e1439fdd523f00e776ad3079698e63
Ethan Brierley <ethanboxx@gmail.com>
2019-10-16 19:10:27 +0100

    DivideByZeroException

---
+++ divide_by_zero_exception/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace divide_by_zero_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int rndNumber = rnd.Next(0, 11);
+            try
+            {
+                double ans = 100 / rndNumber;
+                Console.WriteLine($"100 / {rndNumber} = {ans}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"100 / {rndNumber} Can't be done!");
+            }
+            Thread.Sleep(100);
+            Main(args);
+        }
+    }
+}
