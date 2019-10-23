18b328a5ee985e6f98bf85b47d61519db1bb3c73
Ethan Brierley <ethanboxx@gmail.com>
2019-10-23 12:11:31 +0100

    string functions

---
+++ string_fuctions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace string_fuctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String test = "Hello World!";
+            Console.WriteLine(test.Substring(5).Trim());
+        }
+    }
+}
