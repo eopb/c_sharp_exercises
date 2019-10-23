7885cf13a6515be5570146b1d6ed1a4b21a8446a
Ethan Brierley <ethanboxx@gmail.com>
2019-10-23 12:06:03 +0100

    cleanup

--- random_array/Program.cs
+++ random_array/Program.cs
@@ -8,12 +8,10 @@ namespace random_array
         public static void Main(string[] args)
         {
             Random rnd = new Random();
-            int[] rndNumbers = new int[] { rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101) };
+            int[] rndNumbers = new int[] {
+                rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101) };
             foreach (int i in rndNumbers)
-            {
                 Console.WriteLine($"Random number {i}");
-            }
-
         }
     }
 }
