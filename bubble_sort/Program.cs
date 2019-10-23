a11076bf763563395aaee2c55df242521a77ecc2
Ethan Brierley <ethanboxx@gmail.com>
2019-10-23 13:58:48 +0100

    Cleanup

--- bubble_sort/Program.cs
+++ bubble_sort/Program.cs
@@ -4,32 +4,6 @@ namespace bubble_sort
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            Random rnd = new Random();
-            int[] a = gen_random();
-            System.Console.WriteLine($"length {a.Length}");
-
-            for (bool sorted = false; !sorted; sorted = true)
-            {
-
-                for (int i = 0; i < a.Length - 1; i++)
-                {
-                    int j = i + 1;
-                    if (a[i] < a[i + 1])
-                    {
-                        Console.WriteLine($"{a[i]} {a[j]}");
-                        int temp = a[j];
-                        a[j] = a[i];
-                        a[i] = temp;
-                        sorted = false;
-                    }
-                    print_array(a);
-                }
-            }
-
-
-        }
         public static void print_array(int[] a)
         {
             Console.Write("\n");
@@ -40,13 +14,32 @@ namespace bubble_sort
         public static int[] gen_random()
         {
             Random rnd = new Random();
-            int[] a = new int[8];
+            int[] a = new int[200];
             for (int i = 0; i < a.Length - 1; i++)
             {
                 a[i] = rnd.Next(1, 101);
             }
             return a;
         }
-
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] a = gen_random();
+            bool sorted = false;
+            while (!sorted)
+            {
+                sorted = true;
+                for (int i = 0; i < a.Length - 1; i++)
+                {
+                    int j = i + 1;
+                    if (a[i] < a[j])
+                    {
+                        int t = a[j]; a[j] = a[i]; a[i] = t;
+                        sorted = false;
+                    }
+                    print_array(a);
+                }
+            }
+        }
     }
 }
