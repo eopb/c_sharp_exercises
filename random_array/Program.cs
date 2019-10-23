3cc585b10cf56b1c6e092cb11eb8635f7d8fa556
Ethan Brierley <ethanboxx@gmail.com>
2019-10-23 12:04:55 +0100

    random_array

---
+++ random_array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace random_array
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] rndNumbers = new int[] { rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101), rnd.Next(1, 101) };
+            foreach (int i in rndNumbers)
+            {
+                Console.WriteLine($"Random number {i}");
+            }
+
+        }
+    }
+}
