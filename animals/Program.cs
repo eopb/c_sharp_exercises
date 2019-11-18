2096f76f2a7550657d93d0117a0408192219ce45
Ethan Brierley <ethanboxx@gmail.com>
2019-11-18 11:21:26 +0000

    add animals

---
+++ animals/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace animals
+{
+    interface Animal : Sound, Size { }
+    interface Sound
+    {
+        String noise();
+    }
+    interface Size
+    {
+        String size();
+    }
+    class Dog : Sound, Size, Animal
+    {
+        public String noise() => "Bark";
+        public String size() => "Small";
+
+    }
+    class Cow : Sound, Size, Animal
+    {
+        public String noise() => "Moo";
+        public String size() => "Big";
+    }
+    class Duck : Sound, Size, Animal
+    {
+        public String noise() => "Quack";
+        public String size() => "Very small";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            animals.Add(new Duck()); animals.Add(new Cow()); animals.Add(new Dog());
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"The animal makes the noise {animal.noise()} it is a {animal.size()} animal");
+            }
+        }
+    }
+}
