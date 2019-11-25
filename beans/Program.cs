2f9ced16a03b64b4b1a9402a9c5dd5e1e85e162d
Ethan Brierley <ethanboxx@gmail.com>
2019-11-25 12:14:56 +0000

    beans

---
+++ beans/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace beans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Beans : ICheckOut
+    {
+        public decimal GetPrice() => 2.99M;
+    }
+    public interface ICheckOut
+    {
+        decimal GetPrice();
+    }
+}
