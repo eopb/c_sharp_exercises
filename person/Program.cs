2cb3dfbdf6d12f887f64c1b89c5d58f8bbdf9a08
Ethan Brierley <ethanboxx@gmail.com>
2019-11-25 12:14:30 +0000

    person

---
+++ person/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace person
+{
+    class Person
+    {
+
+        String name;
+        Person(String name)
+        {
+            this.name = name;
+        }
+    }
+    class Student
+    {
+        int studentNumber;
+        Person person;
+        Student(int studentNumber, Person person)
+        {
+            this.studentNumber = studentNumber;
+            this.person = person;
+        }
+
+    }
+    class College
+    {
+        List<Student> students;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
