5cedeb1b63cc36fd782fa9517397eac66b19da09
Ethan Brierley <ethanboxx@gmail.com>
2019-12-06 22:34:19 +0000

    pythagorean triples

---
+++ december/pythagorean-triples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pythagorean_triples
+{
+    class Program
+    {
+        static IEnumerable<string> allTriples()
+        {
+            for (int a = 1; ; a++) for (int b = 1; b <= a; b++)
+                {
+                    var c = Math.Sqrt(a * a + b * b);
+                    if (Math.Floor(c) == c) yield return $"{a},{b},{c}";
+                }
+        }
+        static void Main()
+        {
+            foreach (var s in allTriples()) Console.WriteLine(s);
+        }
+    }
+}
