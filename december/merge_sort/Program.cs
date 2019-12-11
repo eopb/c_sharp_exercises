15aa8359f5b741bdccd9d22fc6f99c76a1cdee79
Ethan Brierley <ethanboxx@gmail.com>
2019-12-11 16:35:46 +0000

    more clean

--- december/merge_sort/Program.cs
+++ december/merge_sort/Program.cs
@@ -8,14 +8,12 @@ namespace merge_sort
     {
         public static List<int> ascending(List<int> a) => s(a, (i, j) => i > j);
         public static List<int> descending(List<int> a) => s(a, (i, j) => i < j);
-        public static List<T> s<T>(List<T> l, Func<T, T, bool> f)
-        {
-            Func<List<T>, List<T>, List<T>> m = (m1, m2) =>
-            {
-                List<T> mo = new List<T>();
+        public static List<T> s<T>(List<T> l, Func<T, T, bool> f) {
+            Func<List<T>, List<T>, List<T>> m = (m1, m2) => {
+                var mo = new List<T>();
                 while (m1.Count > 0 && m2.Count > 0)
                     if (f(m1[0], m2[0])) { mo.Add(m2[0]); m2.RemoveAt(0); }
-                    else { mo.Add(m1[0]); m1.RemoveAt(0); }
+                    else                 { mo.Add(m1[0]); m1.RemoveAt(0); }
                 return mo.Concat(m1).Concat(m2).ToList();
             };
             int n = l.Count;
