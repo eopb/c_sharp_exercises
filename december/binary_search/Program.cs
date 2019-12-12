cf18ac356780bd8e47484a41e581e9f6ead79565
Ethan Brierley <ethanboxx@gmail.com>
2019-12-12 22:06:59 +0000

    binary search

---
+++ december/binary_search/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace binary_search
+{
+    static class binary
+    {
+        public static int search<T>(T[] l, T v) where T : IComparable => s(l, v, 0);
+        static int s<T>(IEnumerable<T> l, T v, int o) where T : IComparable
+        {
+            int hl = l.Count() / 2, c = l.ElementAt(hl).CompareTo(v), of = o + hl;
+            return c == 0 ? of : s(c < 0 ? l.Skip(hl) : l.Take(hl), v, c < 0 ? of : o);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args) =>
+            Console.WriteLine(binary.search(new int[] { 1, 4, 7, 34, 89 }, 89));
+
+    }
+}
